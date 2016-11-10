762bc156bb4b7a4e9b3dfd15bfcd09627117cb3a
Joaquin Diaz <joaquin.diaz@ies.cl>
2016-11-10 19:08:35 -0300

    cambios

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -2,7 +2,15 @@ import { FlowRouter } from 'meteor/kadira:flow-router'
 import { BlazeLayout } from 'meteor/kadira:blaze-layout'
 
 FlowRouter.route('/', {
+  name: 'index',
   action: function () {
     BlazeLayout.render('newlayout', {content: 'newhome'})
+  }
+})
+
+FlowRouter.route('/contact', {
+  name: 'contact',
+  action: function () {
+    BlazeLayout.render('newlayout', {content: 'contact'})
   }
 })
