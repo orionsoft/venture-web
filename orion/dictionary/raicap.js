19354e2529eda3fce67de6514557b3152bc61a94
José Andradez <xjgamx@gmail.com>
2016-11-17 13:38:13 -0300

    adding wysiwig to red-angel and raicap

---
+++ orion/dictionary/raicap.js
@@ -0,0 +1,7 @@
+orion.dictionary.addDefinition('title', 'raicap', {
+    type: String,
+    label: 'Titulo'
+});
+orion.dictionary.addDefinition('body', 'raicap', 
+    orion.attribute('froala')
+    );
