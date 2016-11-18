d8b96e98945458f6998a53a90b648c9313760275
José Andradez <xjgamx@gmail.com>
2016-11-18 18:23:26 -0300

    styling menu for mobile

--- client/views/components/menu/menu.js
+++ client/views/components/menu/menu.js
@@ -1,16 +1,21 @@
 import { Session } from 'meteor/session'
 import { Template } from 'meteor/templating'
 
+Template.menu.onRendered(function () {
+  $('.collapsible').collapsible({accordion: false})
+})
+
 Template.menu.helpers({
-  display () {
+  display() {
     return Session.get('menu')
   },
-  animation () {
+  animation() {
     return Session.get('animation')
   }
 })
+
 Template.menu.events({
-  'click .closeBtn' (event) {
+  'click .closeBtn'(event) {
     Session.set('animation', 'slideOutUp')
   },
   'click .menu a': function (event) {
