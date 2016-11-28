8335344818bae6175502c1bd3fe735fcff5fb2a5
José Andradez <xjgamx@gmail.com>
2016-11-28 17:28:06 -0300

    social buttons prensa

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -6,3 +6,26 @@ reCAPTCHA.config({
   publickey: '6LcfsQwUAAAAAH_9_JNkIQRoj5Xy52yp6kCrCJJj',
   hl: 'es' // optional display language
 })
+SocialButtons.config({
+  facebook: true,
+  twitter: true,
+  google: true,
+  pinterest: true,
+  delicious: false,
+  tumblr: false,
+  pocket: false,
+  reddit: true,
+  linkedin: true,
+  wordpress: false,
+  hackernews: false,
+  stumbleupon: false,
+  digg: false,
+  renren: false,
+  weibo: false,
+  mail: false,
+
+  incrementing: false,
+  useCount: false,
+  incrementerSpeed: 150,
+  via: ''
+});
